7670d3eda0ab627e7f60d2324ebdaea6ac64b458
Kalaidharma <41136461+Kalaidharma@users.noreply.github.com>
2018-07-14 17:17:34 +0530

    Add files via upload

---
+++ Helo.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Helo {
+public static void main(String[] args){
+	Scanner sc=new Scanner(System.in);
+	int N,i;
+	N=sc.nextInt();
+	String a="Hello";
+	for(i=0;i<N;){
+		System.out.println("Hello");
+		i++;
+	}
+	
+}
+}
